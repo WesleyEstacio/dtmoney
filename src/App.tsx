c918ddf2ae90a5dd88b818813bb2e6a1856ccb27
Wesley Estacio <wesleyestacio4@gmail.com>
2022-04-25 08:53:51 -0300

    feat - Mostrando transacoes do banco

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,8 @@ import { GlobalStyle } from './styles/global'
 import Modal from 'react-modal'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard';
-import { createServer } from 'miragejs'
 import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionModal';
-
-createServer({
-  routes(){
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id:1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-  
-})
 
 Modal.setAppElement('#root')
 
